ba819c84521c3604db55b43fd445aa7f62680f59
Ira <irochka.ukhova@gmail.com>
2021-06-24 21:31:50 +0300

    completed task from lesson 9

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 
 console.log('arr'+'- object');
 console.log('4'+'- object');
+console.log(4 + 5);
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(13%2);
+
+console.log(2*8 == 16);
+
+console.log(2*8 === '16');
+
+const isChecked = false,
+      isClosed = false;
+
+console.log(isChecked || isClosed);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+
